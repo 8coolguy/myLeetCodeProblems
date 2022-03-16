e62cf57b81e35c08f411f5e78ef1a4551c6c6b5e
8coolguy <arnavchoudhury20@mittymonarch.com>
2022-03-16 09:34:17 -0700

    Update 997findJudge.py
    finished a graph problem without using graphs

--- 997findJudge.py
+++ 997findJudge.py
@@ -1,20 +1,18 @@
-#not finished this rn
+# got it but I want to do it again using the graph logic
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         arr=[0]*n
-
+        _arr=[0]*n
         for i in trust:
             arr[i[0]-1]+=1
-        res=-1
+            _arr[i[-1]-1]+=1
         count=0
-        print(arr)
+        res=-1
         for i in range(len(arr)):
-            if arr[i]!=n-1 and arr[i]!=0:
-                return -1
-            if arr[i]==0:
+            if arr[i]==0 and _arr[i]==n-1:
                 res=i+1
                 count+=1
-
+            
         if count==1:
             return res
         return -1
