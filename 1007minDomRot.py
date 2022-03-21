b615f1363fbc992a2e00dc37c44a553a13cc4baf
8coolguy <arnavchoudhury20@mittymonarch.com>
2022-03-20 21:37:40 -0700

    another one

--- 1007minDomRot.py
+++ 1007minDomRot.py
@@ -1,19 +1,22 @@
-#did not finsih yet will comeback tommorow 
+#finished this problem you have to calculate the min steps
+class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
         
         top_count={}
         bot_count={}
-        for i in tops:
-            if top_count.__contains__(i):
-                top_count[i]+=1
-            else:
-                top_count[i]=1
-        for j in bottoms:
-            if bot_count.__contains__(j):
-                bot_count[j]+=1
-            else:
-                bot_count[j]=1
-        print(top_count)
-        print(bot_count)
+        same=0
+        for i in range(7):
+            top_count[i+1]=0
+            bot_count[i+1]=0
+        for i in range(len(tops)):
+            top_count[tops[i]]+=1
+            bot_count[bottoms[i]]+=1
+            if bottoms[i] ==tops[i]:
+                same+=1
+        for i in range(7):
+            if top_count[i+1] +bot_count[i+1]- same ==len(tops):
+                return len(tops)-max(top_count[i+1], bot_count[i+1])
+        return -1
             
+        
         
