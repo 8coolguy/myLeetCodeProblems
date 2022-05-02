7afa35ad9a51275dd95aed7bc587ab4c97863584
8coolguy <arnavchoudhury20@mittymonarch.com>
2022-05-02 10:17:32 -0700

    lolz ez

---
+++ 217containsDups.py
@@ -0,0 +1,12 @@
+#they gotta make a too wz category
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        my_dict={}
+        for i in nums:
+            if i in my_dict:
+                return True
+            else:
+                my_dict[i]=1
+        
+        return False
+        
